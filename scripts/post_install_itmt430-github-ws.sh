1db69e25acaaec1a33a0eec368a7126fb1c29011
HRizwan1 <35548223+HRizwan1@users.noreply.github.com>
2019-02-23 00:32:11 -0600

    711

--- scripts/post_install_itmt430-github-ws.sh
+++ scripts/post_install_itmt430-github-ws.sh
@@ -75,10 +75,10 @@ rm -rf /var/www/html/*
 cp -r 2019-team-06f/* /var/www/html
 
 # Blocking public access
-sudo chmod -r 700 /var/www/html/css
-sudo chmod -r 700 /var/www/html/img
-sudo chmod -r 700 /var/www/html/itmt430
-sudo chmod -r 700 /var/www/html/js
-sudo chmod -r 700 /var/www/html/reports
-sudo chmod -r 700 /var/www/html/ResponsiveSlides.js-master
-sudo chmod -r 700 /var/www/html/scripts
+sudo chmod -r 711 /var/www/html/css
+sudo chmod -r 711 /var/www/html/img
+sudo chmod -r 711 /var/www/html/itmt430
+sudo chmod -r 711 /var/www/html/js
+sudo chmod -r 711 /var/www/html/reports
+sudo chmod -r 711 /var/www/html/ResponsiveSlides.js-master
+sudo chmod -r 711 /var/www/html/scripts
