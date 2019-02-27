b542d2318da4e1d91a048a1ff94a9b1761230b8e
Bhumika Patel <45867780+bpatel22@users.noreply.github.com>
2019-02-27 14:29:27 -0600

    checking if things look differnt when shrink-to-fit=no is removed from viewport metatag.

--- gallery.php
+++ gallery.php
@@ -16,7 +16,7 @@
 <head>
     <title>TruHawk Gallery</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <!--  stylesheet -->
     <link rel="stylesheet" type="text/css" href="cssreset.css">
     <link rel="stylesheet" href="css/style.css">
