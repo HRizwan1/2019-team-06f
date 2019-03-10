748737d6bd0ebdb1cdc931743622f742b38082ac
HRizwan1 <35548223+HRizwan1@users.noreply.github.com>
2019-03-09 23:45:14 -0600

    adding sql table pictures

--- itmt430/sql/create-new.sql
+++ itmt430/sql/create-new.sql
@@ -9,3 +9,10 @@ CREATE TABLE users(
   password VARCHAR(100) NOT NULL, 
   PRIMARY KEY(id)
 );
+CREATE TABLE pictures(
+  photo_id INT(11) NOT NULL AUTO_INCREMENT,
+  photo mediumblob NOT NULL,
+  id INT(11) NOT NULL,
+  PRIMARY KEY(photo_id),
+  FOREIGN KEY (id) REFERENCES users(id)
+);
