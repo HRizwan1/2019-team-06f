d3b7738f4c3b43bb450db55f31e3b2b4c4c82cc4
HRizwan1 <35548223+HRizwan1@users.noreply.github.com>
2019-03-11 01:32:43 -0500

    changing user type

--- itmt430/sql/insert-new.sql
+++ itmt430/sql/insert-new.sql
@@ -1,64 +1,64 @@
 Use website;
-INSERT INTO users (id, username, email, password) 
+INSERT INTO users (id, username, user_type, email, password) 
 Values 
   (
-    1, "Ficy1953", "AnitaDByrd@einrot.com", 
+    1, "Ficy1953", "user", "AnitaDByrd@einrot.com", 
     "d05bb31a344150a394a86268b87076a0"
   ), 
   (
-    2, "Nothers", "NoahDVaughn@armyspy.com", 
+    2, "Nothers", "user", "NoahDVaughn@armyspy.com", 
     "0ef2fbb4c70096ae41051a3472677ec6"
   ), 
   (
-    3, "Hurasawends", "JessieCDaughtry@gustr.com", 
+    3, "Hurasawends", "user", "JessieCDaughtry@gustr.com", 
     "e08a980c73fa2b2922082b6ba3115fc6"
   ), 
   (
-    4, "Litend", "BettyCLi@fleckens.hu", 
+    4, "Litend", "user", "BettyCLi@fleckens.hu", 
     "fa89ded6df0b082502ffaf7355041aec"
   ), 
   (
-    5, "Hagerre", "BrianKSifuentes@armyspy.com", 
+    5, "Hagerre", "user", "BrianKSifuentes@armyspy.com", 
     "74f8a94958b2d49e5c35871eac81a2f4"
   ), 
   (
-    6, "Mothesseze", "RobinLShortridge@teleworm.us", 
+    6, "Mothesseze", "user", "RobinLShortridge@teleworm.us", 
     "683b6de824fe701459a4443c9e01d35b"
   ), 
   (
-    7, "Girese", "MittieJReed@armyspy.com", 
+    7, "Girese", "user", "MittieJReed@armyspy.com", 
     "e71fdd54ce0ec6e24b09ede64bf616bf"
   ), 
   (
-    8, "Fement42", "AmyRBabb@jourrapide.com", 
+    8, "Fement42", "user", "AmyRBabb@jourrapide.com", 
     "a572d0f99f033a68fff8ccbc24de477f"
   ), 
   (
-    9, "Withey", "RichardBGerman@jourrapide.com", 
+    9, "Withey", "user", "RichardBGerman@jourrapide.com", 
     "a2f050f50bd36ba1d2064b80a8870047"
   ), 
   (
-    10, "Ther1993", "AlbertREspinoza@teleworm.us", 
+    10, "Ther1993", "user", "AlbertREspinoza@teleworm.us", 
     "e3ac9a0782c90e5de368672eef136d4a"
   ), 
   (
-    11, "Majessis", "EricGBrim@gustr.com", 
+    11, "Majessis", "user", "EricGBrim@gustr.com", 
     "c82de23dcd88a5ee4e6c7c99e76ad7f1"
   ), 
   (
-    12, "Hertake93", "MirnaTRobinson@teleworm.us", 
+    12, "Hertake93", "user", "MirnaTRobinson@teleworm.us", 
     "124c5ad611106d97f9367db137d2c3d5"
   ), 
   (
-    13, "Adven1991", "JenniferJVasquez@superrito.com", 
+    13, "Adven1991", "user", "JenniferJVasquez@superrito.com", 
     "05eae33fcffb03512c4a82a0bf95e3b1"
   ), 
   (
-    14, "Highted", "JenniferTStarbuck@einrot.com", 
+    14, "Highted", "user", "JenniferTStarbuck@einrot.com", 
     "3443a6bdb6614571826b39dd7cbf23f5"
   ), 
   (
-    15, "Obect1934", "RussellEDaniels@rhyta.com", 
+    15, "Obect1934", "user", "RussellEDaniels@rhyta.com", 
     "37cf97aa4102f9933ad033523f71766e"
   );
 INSERT INTO pictures (photo_id, photo, id) 
