078c4444dd4d391f0bda5f24e50e992b99ce523c
jdsouz7 <jdsouz7@gmail.com>
2019-03-12 00:11:53 -0500

    updating to make images responsive and scale down

--- register.php
+++ register.php
@@ -21,7 +21,7 @@
 
         <!--  main  -->
         <a id="logo" href="index.php">
-          <img src="img/THLogo.png" alt="THLogo">
+          <img src="img/THLogo.png" alt="THLogo" style="max-width:100%;height:auto;">
           <span>TruHawk</span>
         </a>
 
@@ -71,7 +71,7 @@
 
 <!-- the footer -->
 <footer>
-  <img src="img/THLogo.png" alt="THLogo">
+  <img src="img/THLogo.png" alt="THLogo" style="max-width:100%;height:auto;">
   <span>&copy; 2019 TruHawk </span>
 </footer>
 
