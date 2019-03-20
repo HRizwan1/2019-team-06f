fc5da96bba9189a1fe101f31e59b156db0473a2e
HRizwan1 <35548223+HRizwan1@users.noreply.github.com>
2019-03-19 21:34:42 -0500

    fixing create user search bar

--- admin/create_user.php
+++ admin/create_user.php
@@ -23,8 +23,8 @@ if (isset($_GET['logout'])) {
     <meta charset="utf-8" class="nojs">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <!--  stylesheet -->
-    <link rel="stylesheet" type="text/css" href="../cssreset.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/cssreset.css">
+    <link rel="stylesheet" href="../css/style1.css">
     <link rel="shortcut icon" href="img/THLogo.ico" />
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script src="ResponsiveSlides.js-master/responsiveslides.min.js"></script>
@@ -36,18 +36,16 @@ if (isset($_GET['logout'])) {
         <!--  main  -->
         <a id="logo" href="index.php">
             <img src="../img/THLogo.png" alt="THLogo">
-			<span><font size="5">TruHawk</font></span>
+			<span>TruHawk</span>
 			
 			
         </a>
 
-        
+        <input type="text" class="searchTerm" placeholder="Search for hashtag...">
 
         <!--  navigation  -->
         <nav>
             <ul>
-				
-            <input type="text" class="searchTerm" placeholder="Search..">
 				<li><a href="index.php">Home</a></li>
 				<li><a href="admin.php">Admin</a></li>
                 <li><a href="about.php">About</a></li>
