1199cfb7a014f29e060b16f98540aa7cfefd5cf2
dkolov84 <31516121+dkolov84@users.noreply.github.com>
2019-04-03 22:08:07 -0500

    added searchbar

--- user/searchresults.php
+++ user/searchresults.php
@@ -57,16 +57,22 @@ if (isset($_GET['logout'])) {
 
         <!--  navigation  -->
         <nav>
-            <ul>
-				
-            <input type="text" class="searchTerm" placeholder="Search..">
-				<li><a href="index.php">Home</a></li>
-				<li><a href="user.php">User</a></li>
+            <ul>		
+                <li>
+		   <form id="search" action="searchresults.php" value="<?php echo $search; ?>" method="POST">
+		     <div class="search-box" id="search-box"> 
+		     <input type="text" id="searchbar" name="searchtext" class="search-txt" placeholder="Enter tag and press Enter to search"/> 
+		     <button type="submit" class="search-btn" name="search-btn"> 
+		     <i class="fas fa-search"></i> 
+		     </button>
+		     </div>
+		   <form>
+		</li> 
+		<li><a href="index.php">Home</a></li>
+		<li><a href="user.php">User</a></li>
                 <li><a href="about.php">About</a></li>
-				<li><a href="gallery.php">Photo Gallery</a></li>
-				 <li><a href="logout.php">Logout</a></li>
-
-
+		<li><a href="gallery.php">Photo Gallery</a></li>
+	        <li><a href="logout.php">Logout</a></li>
             </ul>
 		</nav>
 
