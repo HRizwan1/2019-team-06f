d37d0ff13105a582fbeeabb189625cbd6b3ea094
HRizwan1 <35548223+HRizwan1@users.noreply.github.com>
2019-04-04 14:50:01 -0500

    fixed shan's view user table

--- admin/view_user.php
+++ admin/view_user.php
@@ -25,7 +25,7 @@ if (isset($_GET['logout'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <!--  stylesheet -->
     <link rel="stylesheet" type="text/css" href="../cssreset.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style2.css">
     <link rel="shortcut icon" href="img/THLogo.ico" />
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script src="ResponsiveSlides.js-master/responsiveslides.min.js"></script>
@@ -37,18 +37,17 @@ if (isset($_GET['logout'])) {
         <!--  main  -->
         <a id="logo" href="index.php">
             <img src="../img/THLogo.png" alt="THLogo">
-			<span><font size="5">TruHawk</font></span>
+			<span>TruHawk</span>
 			
 			
         </a>
 
-        
+        <input type="text" class="searchTerm" placeholder="Search for hashtag...">
 
         <!--  navigation  -->
         <nav>
             <ul>
 				
-            <input type="text" class="searchTerm" placeholder="Search..">
 				<li><a href="index.php">Home</a></li>
 				<li><a href="admin.php">Admin</a></li>
                 <li><a href="about.php">About</a></li>
