112440edf84ea0b570b804ba7a2c685582f33bd2
dkolov84 <31516121+dkolov84@users.noreply.github.com>
2019-04-15 19:34:15 -0500

    /..

--- code/user/gallery.php
+++ code/user/gallery.php
@@ -52,7 +52,7 @@ if (isset($_GET['logout'])) {
 		   <i class="fas fa-search"></i> 
 		   </button>
 		   </div>
-		   <form>
+		   </form>
 		</li> 
                 <li><a href="index.php">Home</a></li>
                 <li><a href="user.php">User</a></li>
